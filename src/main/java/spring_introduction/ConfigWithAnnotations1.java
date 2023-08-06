04481f0a71f82cb1f451980a34b8950f60f6cd1d
polinadavidenko <davidenkopk@gmail.com>
2023-08-06 23:50:57 +0300

    add new annotation

--- src/main/java/spring_introduction/ConfigWithAnnotations1.java
+++ src/main/java/spring_introduction/ConfigWithAnnotations1.java
@@ -5,8 +5,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ConfigWithAnnotations1 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext3.xml");
-        Cat myCat = context.getBean("catBean", Cat.class);
-        myCat.Say();
+        Person person = context.getBean("personBean", Person.class);
+        person.callYouretPet();
+
+
+//        Cat myCat = context.getBean("catBean", Cat.class);
+//        myCat.Say();
         context.close();
     }
 }
--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -1,10 +1,22 @@
 package spring_introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("personBean")
 public class Person {
 
     private Pet pet;
     private  String surname;
     private int age;
+
+    @Autowired
+    public Person(Pet pet) {
+        System.out.println("Person been is created");
+        this.pet = pet;
+    }
+
+//    public Person() {}
 
     public String getSurname() {
         return surname;
@@ -23,16 +35,6 @@ public class Person {
         System.out.println("Class age: set age");
         this.age = age;
     }
-
-//    public Person(Pet pet) {
-//        this.pet = pet;
-//    }
-
-
-    public Person() {
-        System.out.println("Person been is created");
-    }
-
 
     public void setPet(Pet pet) {
         System.out.println("set pet");
