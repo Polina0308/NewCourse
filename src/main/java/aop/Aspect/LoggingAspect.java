0e3c30f3d0d085916cf2fdbbe9332c436333e01a
polinadavidenko <davidenkopk@gmail.com>
2023-08-10 15:50:09 +0300

    add advice type before

--- src/main/java/aop/Aspect/LoggingAspect.java
+++ src/main/java/aop/Aspect/LoggingAspect.java
@@ -6,9 +6,14 @@ import org.aspectj.lang.annotation.Aspect;
 @Component
 @Aspect
 public class LoggingAspect {
-@Before("execution(public  void getBook())")
+@Before("execution(public  void get*())")
     public  void beforeGetBookAdvice(){
         System.out.println("beforeGetBookAdvice: попытка получить книгу");
+    }
+
+    @Before("execution( * ReturnBook())")
+    public  void beforeReturnBookAdvice(){
+        System.out.println("beforeReturnBookAdvice: попытка вернуть книгу");
     }
 
 }
